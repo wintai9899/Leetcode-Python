e74fbf586f87863a9e20cd37b74f8336a0bc3e32
wintai9899 <wintai9899@163.com>
2023-02-28 18:05:09 +0000

    First commit

---
+++ Graph/200.number-of-islands.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=200 lang=python3
+#
+# [200] Number of Islands
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        islands = 0
+        visited = set()
+        
+        def dfs(r,c):
+            if r == ROWS or r < 0 or c == COLS or c < 0 or grid[r][c] == "0" or (r,c) in visited:
+                return 0 
+            visited.add((r,c))
+            
+            return dfs(r+1,c) + dfs(r-1,c) + dfs(r,c+1) + dfs(r,c-1) + 1
+            
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    dfs(r,c)
+                    islands += 1 
+        
+        return islands 
+        
+        
+            
+        
+        
+# @lc code=end
