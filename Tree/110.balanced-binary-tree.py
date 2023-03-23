023a06f8c63c19195d9ac41907dacc454bbb35f2
wintai9899 <wintai9899@163.com>
2023-03-23 15:34:18 +0000

    tree problems

---
+++ Tree/110.balanced-binary-tree.py
@@ -0,0 +1,76 @@
+#
+# @lc app=leetcode id=110 lang=python3
+#
+# [110] Balanced Binary Tree
+#
+# https://leetcode.com/problems/balanced-binary-tree/description/
+#
+# algorithms
+# Easy (48.92%)
+# Likes:    8566
+# Dislikes: 486
+# Total Accepted:    1.1M
+# Total Submissions: 2.2M
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, determine if it is height-balanced.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: root = [3,9,20,null,null,15,7]
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: root = [1,2,2,3,3,null,null,4,4]
+# Output: false
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: root = []
+# Output: true
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# The number of nodes in the tree is in the range [0, 5000].
+# -10^4 <= Node.val <= 10^4
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        if not root:
+            return True
+
+        heightDiff = abs(self.getPathLength(root.left) - self.getPathLength(root.right))
+        
+        return heightDiff <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
+    
+    def getPathLength(self, node):
+        if not node:
+            return 0 #
+
+        leftPath = self.getPathLength(node.left)
+        rightPath = self.getPathLength(node.right)
+        
+        return max(leftPath, rightPath) + 1
+# @lc code=end
+
